6b89366a30fb49e4401a6658ec55f6580686e43a
lxwttt <45623730+lxwttt@users.noreply.github.com>
2025-01-26 13:53:18 +0800

    Update ukf.h

--- new_armor_tracker/include/new_armor_tracker/kalman/filter/ukf.h
+++ new_armor_tracker/include/new_armor_tracker/kalman/filter/ukf.h
@@ -4,9 +4,8 @@
 #include <Eigen/Dense>
 #include <cmath>
 
-template <int dimX, int dimY>
-class UKF {
- public:
+template <int dimX, int dimY> class UKF {
+public:
   using MatXX = Eigen::Matrix<double, dimX, dimX>;
   using MatXY = Eigen::Matrix<double, dimX, dimY>;
   using MatYX = Eigen::Matrix<double, dimY, dimX>;
@@ -15,37 +14,19 @@ class UKF {
   using VecY = Eigen::Matrix<double, dimY, 1>;
 
   UKF()
-      : estimate_X(VecX::Zero()),
-        P(MatXX::Identity()),
-        Q(MatXX::Identity()),
-        R(MatYY::Identity()),
-        lambda(3 - dimX) {
-    weights_mean[0] = lambda / (dimX + lambda);
-    weights_cov[0] = weights_mean[0] + (1.0 - alpha * alpha + beta);
-
-    for (int i = 1; i < 2 * dimX + 1; i++) {
-      weights_mean[i] = 1.0 / (2.0 * (dimX + lambda));
-      weights_cov[i] = weights_mean[i];
-    }
+      : estimate_X(VecX::Zero()), P(MatXX::Identity()), Q(MatXX::Identity()),
+        R(MatYY::Identity()), lambda(3 - dimX),
+        forgetting_factor(0.3) { // Default forgetting factor
+    initializeWeights();
   }
 
-  UKF(const MatXX& Q0, const MatYY& R0, double alpha = 1e-3, double beta = 2.0,
-      double kappa = 0.0)
-      : estimate_X(VecX::Zero()),
-        P(MatXX::Identity()),
-        Q(Q0),
-        R(R0),
-        alpha(alpha),
-        beta(beta),
-        kappa(kappa),
-        lambda(alpha * alpha * (dimX + kappa) - dimX) {
-    weights_mean[0] = lambda / (dimX + lambda);
-    weights_cov[0] = weights_mean[0] + (1.0 - alpha * alpha + beta);
-
-    for (int i = 1; i < 2 * dimX + 1; i++) {
-      weights_mean[i] = 1.0 / (2.0 * (dimX + lambda));
-      weights_cov[i] = weights_mean[i];
-    }
+  UKF(const MatXX &Q0, const MatYY &R0, double alpha = 1e-3, double beta = 2.0,
+      double kappa = 0.0, double forgetting_factor = 0.3)
+      : estimate_X(VecX::Zero()), P(MatXX::Identity()), Q(Q0), R(R0),
+        alpha(alpha), beta(beta), kappa(kappa),
+        lambda(alpha * alpha * (dimX + kappa) - dimX),
+        forgetting_factor(forgetting_factor) {
+    initializeWeights();
   }
 
   void restart() {
@@ -53,23 +34,21 @@ class UKF {
     P = MatXX::Identity();
   }
 
-  VecX estimate_X;
-  VecX predict_X;
-  VecY predict_Y;
-  MatXX P;
-  MatXX Q;
-  MatYY R;
+  // Adaptive noise adjustment method
+  void adaptNoise(const VecY &innovation) {
+    // Update R adaptively based on innovation
+    R = (1.0 - forgetting_factor) * R +
+        forgetting_factor * (innovation * innovation.transpose());
 
-  // Parameters for sigma points
-  double alpha;   // Spread of sigma points, usually a small positive value
-  double beta;    // Optimal value is 2 for Gaussian distributions
-  double kappa;   // Secondary scaling parameter
-  double lambda;  // Scaling factor for sigma points
-  Eigen::Matrix<double, 2 * dimX + 1, 1> weights_mean;
-  Eigen::Matrix<double, 2 * dimX + 1, 1> weights_cov;
+    // Update Q adaptively (basic implementation, can be application-specific)
+    MatXX state_diff = MatXX::Zero();
+    for (int i = 0; i < dimX; ++i) {
+      state_diff(i, i) = std::pow(innovation.norm(), 2);
+    }
+    Q = (1.0 - forgetting_factor) * Q + forgetting_factor * state_diff;
+  }
 
-  template <class Func>
-  VecX predict(Func&& func) {
+  template <class Func> VecX predict(Func &&func) {
     auto sigma_points = generateSigmaPoints();
     Eigen::Matrix<double, dimX, 2 * dimX + 1> predicted_sigma_points;
 
@@ -95,8 +74,7 @@ class UKF {
     return predict_X;
   }
 
-  template <class Func>
-  VecX update(Func&& func, const VecY& Y) {
+  template <class Func> VecX update(Func &&func, const VecY &Y) {
     auto sigma_points = generateSigmaPoints();
     Eigen::Matrix<double, dimY, 2 * dimX + 1> predicted_measurements;
 
@@ -131,13 +109,27 @@ class UKF {
     MatXY K = cross_cov * S.inverse();
 
     // Update state estimate and covariance
-    estimate_X = predict_X + K * (Y - predict_Y);
+    VecY innovation = Y - predict_Y;
+    estimate_X = predict_X + K * innovation;
     P = P - K * S * K.transpose();
+
+    // Adaptively adjust noise covariance matrices
+    adaptNoise(innovation);
 
     return estimate_X;
   }
 
- private:
+private:
+  void initializeWeights() {
+    weights_mean[0] = lambda / (dimX + lambda);
+    weights_cov[0] = weights_mean[0] + (1.0 - alpha * alpha + beta);
+
+    for (int i = 1; i < 2 * dimX + 1; i++) {
+      weights_mean[i] = 1.0 / (2.0 * (dimX + lambda));
+      weights_cov[i] = weights_mean[i];
+    }
+  }
+
   Eigen::Matrix<double, dimX, 2 * dimX + 1> generateSigmaPoints() {
     Eigen::Matrix<double, dimX, 2 * dimX + 1> sigma_points;
     MatXX sqrt_P = P.llt().matrixL();
@@ -152,6 +144,23 @@ class UKF {
 
     return sigma_points;
   }
+
+public:
+  VecX estimate_X;
+  VecX predict_X;
+  VecY predict_Y;
+  MatXX P;
+  MatXX Q;
+  MatYY R;
+
+  double alpha;             // Spread of sigma points
+  double beta;              // Optimized for Gaussian distributions
+  double kappa;             // Secondary scaling parameter
+  double lambda;            // Scaling factor
+  double forgetting_factor; // Forgetting factor for adaptive noise
+
+  Eigen::Matrix<double, 2 * dimX + 1, 1> weights_mean;
+  Eigen::Matrix<double, 2 * dimX + 1, 1> weights_cov;
 };
 
 #endif
